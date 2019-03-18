4a04abcb013083bf222ffd070d8a25f55da8ce71
Yuan Zhen <SENSETIME\yuanzhen@g114e1900394.domain.sensetime.com>
2019-03-18 19:52:13 +0800

    start

---
+++ src/main/java/com/yz/web/Hello.java
@@ -0,0 +1,15 @@
+package com.yz.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "hello";
+    }
+
+
+}
